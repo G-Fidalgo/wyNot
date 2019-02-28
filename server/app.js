0edd4d6361453f24556a08e462aa8979804dc467
Mr.X <seniorito.x@icloud.com>
2019-02-28 12:44:23 +0100

    deploy

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/server', {useNewUrlParser: true})
+  .connect(`${DB_URL}`, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
