4e08ead59df41cfd79c15f998e9075dc79bbbd08
Mr.X <seniorito.x@icloud.com>
2019-03-04 10:37:56 +0100

    log in done

--- server/app.js
+++ server/app.js
@@ -3,11 +3,15 @@ require('dotenv').config();
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+
+const session      =require('express-session');
+const MongoStore   =require('connect-mongo')(session)
+const cors         =require('cors')
+const passport     =require('passport')
+
 
 
 mongoose
@@ -23,12 +27,22 @@ const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+app.use(
+  cors({
+    credentials:true,
+    origin:['http://localhost:3000']
+  })
+)
+
 
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // Express View engine setup
 
@@ -40,7 +54,6 @@ app.use(require('node-sass-middleware')({
       
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -48,10 +61,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+//Enable authentication using session passport
+app.use(session({
+  secret: 'ines&gonzalo app',
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({ mongooseConnection: mongoose.connection})
+
+}))
+
 
 
 const index = require('./routes/index');
 app.use('/', index);
+
+const auth = require('./routes/auth')
+app.use('/auth', auth)
 
 
 module.exports = app;
